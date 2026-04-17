1916e76dca4f157decf319974c49629b787a37d6
junaidsa <94611843+junaidsa@users.noreply.github.com>
2026-04-17 12:44:28 +0500

    add portfolio code

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import html from "./tech/html.png";
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import javascript from "./tech/javascript.png";
+import docker from "./tech/docker.png";
+import laravel from "./tech/laravel.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import vue from "./tech/vue.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import python from "./tech/python.png";
+import ai from "./tech/ai.png";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/crexed.webp";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  python,
+  ai,
+  javascript,
+  laravel,
+  nodejs,
+  reactjs,
+  vue,
+  tailwind,
+  typescript,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+};
